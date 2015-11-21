abdea1dda72f664e26e1db8955b0570701f8eb8b
Chris Ramón <rchristian.ramon@gmail.com>
2015-11-21 00:11:57 -0500

    rename rootHandler to IndexHandler

--- store/main.go
+++ store/main.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Store App!"))
 }
 
 func main() {
 	log.Println("server running on :8080")
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", IndexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
