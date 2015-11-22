b4a1d30de3c9a5c020088d89667521a2912e33ea
Chris Ramón <rchristian.ramon@gmail.com>
2015-11-21 19:11:15 -0500

    adds step-2 code

--- store/main.go
+++ store/main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Store App!"))
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	html := "<h1>{{.Title}}<h1/>"
+	t, err := template.New("index").Parse(html)
+	if err != nil {
+		log.Printf("failed to parse index template, error: %v", err)
+		return
+	}
+	data := map[string]string{
+		"Title": "Store App! :)",
+	}
+	if err := t.Execute(w, data); err != nil {
+		log.Printf("failed to execute index template, error: %v", err)
+		return
+	}
 }
 
 func main() {
 	log.Println("server running on :8080")
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", IndexHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
